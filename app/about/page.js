084860bab9d1d3a0ff8402f8f4605de13baf4b77
Bryan <bryanedman86@gmail.com>
2024-06-08 15:24:26 -0600

    ok hopefully that fixed things

--- app/about/page.js
+++ app/about/page.js
@@ -1,11 +1,14 @@
-import React from 'react'
+import React from "react";
 
 const About = () => {
   return (
     <div>
-        <h1>This is my number Manipulator website, it's just a personal project for learning full stack web development!</h1>
+      <h1>
+        This is my number Manipulator website, it&apos;s just a personal project
+        for learning full stack web development!
+      </h1>
     </div>
-  )
-}
+  );
+};
 
-export default About
+export default About;
