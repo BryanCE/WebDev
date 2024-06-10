19ae44ef35cc9c3e1701e36a08cb140a059a994f
Bryan <bryanedman86@gmail.com>
2024-06-09 23:13:52 -0600

    IDK

--- app/lib/auth.js
+++ app/lib/auth.js
@@ -4,7 +4,6 @@ import { redirect } from "next/navigation";
 
 import CredentialsProvider from "next-auth/providers/credentials";
 import GoogleProvider from "next-auth/providers/google";
-import GithubProvider from "next-auth/providers/github";
 
 export const authConfig = {
   providers: [
